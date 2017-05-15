d3debf61348a2c8b8315dac0286751f95c2ddd4e
stereomatchingkiss <thamngapwei@gmail.com>
2017-05-15 16:16:49 +0800

    add comments

--- network/download_supervisor.hpp
+++ network/download_supervisor.hpp
@@ -24,32 +24,28 @@ class download_supervisor : public QObject
 {
     Q_OBJECT
 public:
-    struct download_task
-    {
-        friend class download_supervisor;
-
-        size_t get_unique_id() const;
-        QUrl get_url() const;
-
-    private:
-        QByteArray data_;        
-        QFile file_;
-        bool file_can_open_ = true;
-        QNetworkReply::NetworkError network_error_code_ = QNetworkReply::NoError;
-        QNetworkReply *network_reply_ = nullptr;
-        QNetworkRequest network_request_;
-        QString save_at_;
-        bool save_as_file_ = true;
-        size_t unique_id_ = 0;
-        QUrl url_;
-    };
-
     explicit download_supervisor(QObject *parent = nullptr);
 
-    size_t append(QNetworkRequest const &url, QString const &save_at, bool save_as_file = true);
+    /**
+      * Append new data to the download list and begin to download
+      * @param request url of the data want to download
+      * @param save_at the location you want to save the file at
+      * @param save_as the file name of the download target
+      * @return unique id for each download request
+      */
+    size_t append(QNetworkRequest const &request, QString const &save_at, bool save_as_file = true);
 
+    /**
+      * This value determine how many items could be downloaded
+      * at the same time
+      * @return maximum download size
+      */
     size_t get_max_download_file() const;
 
+    /**
+      * Set maximum download size
+      * @param maximum download size
+      */
     void set_max_download_file(size_t val);
 
 signals:
@@ -66,6 +62,25 @@ private slots:
     void ready_read();
 
 private:    
+    struct download_task
+    {
+        friend class download_supervisor;
+
+        size_t get_unique_id() const;
+        QUrl get_url() const;
+
+    private:
+        QByteArray data_;
+        QFile file_;
+        bool file_can_open_ = true;
+        QNetworkReply::NetworkError network_error_code_ = QNetworkReply::NoError;
+        QNetworkReply *network_reply_ = nullptr;
+        QNetworkRequest network_request_;
+        QString save_at_;
+        bool save_as_file_ = true;
+        size_t unique_id_ = 0;
+    };
+
     void download_start(std::shared_ptr<download_task> &task);
     void launch_download_task(std::shared_ptr<download_task> &task);
     QString save_file_name(download_task const &task) const;
@@ -76,7 +91,7 @@ private:
     QNetworkAccessManager *network_access_;
     std::map<QNetworkReply*, std::shared_ptr<download_task>> reply_table_;
     size_t total_download_file_;
-    size_t unique_id_;    
+    size_t unique_id_;
 };
 
 } //namespace net
