41e9ebf1d43a914baffe3697b73f8159367be572
stereomatchingkiss <thamngapwei@gmail.com>
2017-06-01 21:05:15 +0800

    first commit qte_utility

---
+++ utility/qte_utility.cpp
@@ -0,0 +1,33 @@
+#include "qte_utility.hpp"
+
+#include <QFile>
+#include <QFileInfo>
+#include <QRegularExpression>
+
+namespace qte{
+
+namespace utils{
+
+QString unique_file_name(QString const &save_at, QString const &file_name)
+{
+    QRegularExpression const re("[<>:\"/\\*\\?\\|\\\\]");
+    auto valid_file_name = file_name;
+    valid_file_name = valid_file_name.remove(re).trimmed();
+    if(QFile::exists(save_at + "/" + file_name)){
+        QFileInfo file_info(valid_file_name);
+        QString const base_name = file_info.baseName();
+        QString complete_suffix = file_info.completeSuffix();
+        QString new_file_name = base_name + "(0)." + complete_suffix;
+        for(size_t i = 1; QFile::exists(save_at + "/" + new_file_name); ++i){
+            new_file_name = base_name + "(" + QString::number(i) + ")." + complete_suffix;
+        }
+
+        return new_file_name.trimmed();
+    }
+
+    return file_name;
+}
+
+}
+
+}
---
+++ utility/qte_utility.hpp
@@ -0,0 +1,16 @@
+#ifndef QTE_UTILS_QTE_UTILITY_HPP
+#define QTE_UTILS_QTE_UTILITY_HPP
+
+#include <QString>
+
+namespace qte{
+
+namespace utils {
+
+QString unique_file_name(QString const &save_at, QString const &file_name);
+
+}
+
+}
+
+#endif // UTILITY_HPP
